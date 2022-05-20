d9fd20617b0a9e2c84b891960fede27f4f958c00
Jessica Rash <rashfamily2000@yahoo.com>
2022-05-20 17:24:37 -0400

    Second Commit

--- knowledge_check_1.py
+++ knowledge_check_1.py
@@ -1,2 +1,7 @@
 print('\tHello World!\n',"\tHope you're having a great day!")
 
+cars =[{'Make' : 'Chevy', 'Model' : 'Bolt EUV', 'Year' : 2022},
+    {'Make' : 'Kia' , 'Model' : 'Soul' , 'Year' : 2022},
+    {'Make' : 'Kia' , 'Model' : 'Rio' , 'Year' : 2021} ]
+    
+print(cars[1])
