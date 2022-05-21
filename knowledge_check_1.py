edc211a9d7cec9b9167e108a1041c2b62aa072d8
Jessica Rash <rashfamily2000@yahoo.com>
2022-05-21 11:40:47 -0400

    Updated File

--- knowledge_check_1.py
+++ knowledge_check_1.py
@@ -1,11 +1,29 @@
+#Statement to print hello world
 print('\tHello World!\n',"\tHope you're having a great day!")
-<<<<<<< HEAD
 
-cars =[{'Make' : 'Chevy', 'Model' : 'Bolt EUV', 'Year' : 2022},
-    {'Make' : 'Kia' , 'Model' : 'Soul' , 'Year' : 2022},
-    {'Make' : 'Kia' , 'Model' : 'Rio' , 'Year' : 2021} ]
-    
+#Populated list
+cars = [
+    {'Make' : 'Kia', 'Model' : 'Soul', 'Year' : 2022},
+    {'Make' :'Kia', 'Model': 'Rio', 'Year' : 2021},
+    {'Make' : 'Chevy', 'Model': 'Bolt EUV', 'Year' : 2022}
+]
+#Value from populated list
 print(cars[1])
-=======
-#Testing
->>>>>>> 64f1cdbbcf299b5ff06c172b58fdee0563b27b02
+
+#Type for cars
+print(type(cars[0]))
+
+#Making a dictionary with 2 keys and values
+for item in cars:
+    print(item['Make'], item['Year'])
+    
+
+for item in cars:
+    print(item['Make'])
+
+#Tuple
+storm = ('rain', 'thunder', 'hail', 'sleet')
+print(storm[2]) 
+
+
+
